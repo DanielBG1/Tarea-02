7ffdcd6f5021b4d1429a1b8bc93729a199d36847
USUARIO <USUARIO@172.16.100.49>
2022-02-17 21:43:29 -0600

    Tarea 02 de Desarrollo de Aplicaciones Web y Patrones, Página LuxuryCars

---
+++ src/main/java/com/luxurycars/LuxurycarsApplication.java
@@ -0,0 +1,13 @@
+package com.luxurycars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxurycarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxurycarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luxurycars/LuxurycarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxurycars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxurycarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
